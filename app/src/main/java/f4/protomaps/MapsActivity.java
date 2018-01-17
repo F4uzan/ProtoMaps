12c6a020bdf8e8ea3e014712f5a75fa4299ab43e
F4uzan <f4uzan@this.is.f4>
2018-01-17 15:07:58 +0000

    MapsActivity: Prompt for location permission
    Fix location breaking due to an ungranted location permission.

--- app/src/main/java/f4/protomaps/MapsActivity.java
+++ app/src/main/java/f4/protomaps/MapsActivity.java
@@ -1,6 +1,11 @@
 package f4.protomaps;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.CookieManager;
@@ -13,11 +18,19 @@ import android.webkit.WebViewClient;
 
 public class MapsActivity extends AppCompatActivity {
 
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+
+    Boolean mLocationPermissionGranted = false;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+        getLocationPermission();
         loadMaps();
     }
 
@@ -46,5 +59,41 @@ public class MapsActivity extends AppCompatActivity {
 
         // Load our PWA maps link
         mapsWebView.loadUrl("https://www.google.com/maps?force=qVTs2FOxxTmHHo79-pwa&amp;source=mlapk");
+    }
+
+    private void getLocationPermission(){
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                    COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mLocationPermissionGranted = true;
+            } else {
+                ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        } else {
+            ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    for (int grantResult : grantResults) {
+                        if (grantResult != PackageManager.PERMISSION_DENIED) {
+                            mLocationPermissionGranted = false;
+                            return;
+                        }
+                    }
+                    mLocationPermissionGranted = true;
+                }
+            }
+        }
     }
 }
