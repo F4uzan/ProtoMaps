c7a2801e827178a1933fab8c587884af55c857f0
F4uzan <f4uzan@this.is.f4>
2018-01-17 14:44:12 +0000

    Push the files!
    This is a working and buildable copy of the app. There needs to be a few improvements to be made, but this should work fine for a first commit.

---
+++ app/src/main/java/f4/protomaps/MapsActivity.java
@@ -0,0 +1,43 @@
+package f4.protomaps;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MapsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        loadMaps();
+    }
+
+    public void loadMaps() {
+        WebView mapsWebView = findViewById(R.id.mapsView);
+        mapsWebView.setWebChromeClient(new WebChromeClient());
+        mapsWebView.setWebViewClient(new WebViewClient());
+
+        // Tell the WebView that we accept cookies
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            CookieManager.getInstance().setAcceptThirdPartyCookies(mapsWebView, true);
+            CookieManager.allowFileSchemeCookies();
+        } else {
+            CookieManager.getInstance().setAcceptCookie(true);
+        }
+
+        // Accept JS
+        WebSettings webSettings = mapsWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // Load our PWA maps link
+        mapsWebView.loadUrl("https://www.google.com/maps?force=qVTs2FOxxTmHHo79-pwa&amp;source=mlapk");
+    }
+}
